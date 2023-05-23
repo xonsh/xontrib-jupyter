62f061bc6739374c7a3c1172901088516f9d1959
Andy Kipp <anki-code@users.noreply.github.com>
2023-05-23 17:36:02 +0600

    Update kernel.py

--- xonsh_jupyter/kernel.py
+++ xonsh_jupyter/kernel.py
@@ -493,6 +493,7 @@ def main():
         shell_type=JupyterShell,
         env={
             "PAGER": "cat",
+            "AWS_PAGER": "cat",  # https://docs.aws.amazon.com/cli/latest/userguide/cli-usage-pagination.html#cli-usage-pagination-awspager
             "XONSH_CAPTURE_ALWAYS": True,
         },
         aliases={"less": "cat"},
